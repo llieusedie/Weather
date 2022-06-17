387f8bca2591823659e3707b29795a82591bca49
llieusedie <90383958+llieusedie@users.noreply.github.com>
2022-06-17 20:49:59 +0300

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by Paul Kirnoz on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
